c1eaab15dba91856851c5abac75ef3269bae2fa4
Valeria Soares <valeriasoaressantos2@gmail.com>
2025-04-26 10:37:31 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const images = [
+  'url("assets/na_comunidade_1.jpg")',
+  'url("assets/na_comunidade_2.jpg")',
+  'url("assets/na_comunidade_3.jpg")',
+  'url("assets/na_comunidade_4.jpg")',
+];
+
+let currentIndex = 0;
+const backgroundContainer = document.querySelector("#sec-5 .container .container-img ");
+
+function changeBackgroundImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  backgroundContainer.style.backgroundImage = images[currentIndex];
+}
+
+setInterval(changeBackgroundImage, 2500);
+
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3, 
+  spaceBetween: 20, 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: { 
+      slidesPerView: 1,
+      spaceBetween: 60, 
+    },
+    768: { 
+      slidesPerView: 2,
+      spaceBetween: 80,
+    },
+    1024: { 
+      slidesPerView: 3,
+      spaceBetween: 20,
+    }
+  }
+});
